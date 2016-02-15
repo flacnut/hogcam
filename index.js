e152b406bde09a36bb0d87a0074c82e9814359dc
Adam Sheldon <adam.sheldon@hotmail.com>
2016-02-14 19:10:41 -0800

    Fixed: logging.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var gpio  = require('rpi-gpio');
 var shell = require('shelljs');
 
 var STORAGE_DESTINATION = '/mnt/nas',
-    TEMP_STORAGE_DESTINATION = './temp_vid',
+    TEMP_STORAGE_DESTINATION = '/home/pi/temp_vid',
     DURATION = 1 * 60 * 1000,
     BITRATE = 2 * 1000 * 1000,
     DATE_SETTINGS = {
@@ -27,14 +27,21 @@ gpio.on('change', (chanel, value) => {
 
 // Find out how many ticks we are from the next 10 minute interval
 var offset = DURATION - (Date.now() % (DURATION));
-console.log(`Offset of ${offset / 1000} seconds detected, waiting...`);
+console.log(`[${new Date()}] Offset of ${offset / 1000} seconds detected, waiting...`);
 
 // Start!
 setTimeout(captureLoop, offset);
 
+
+
 /*
  * Helper Functions
  */
+
+function captureLoop() {
+  captureSingleVideo();
+  setInterval(captureSingleVideo, DURATION);
+}
 
 function captureSingleVideo() {
   var TIMESTAMP = new Date()
@@ -45,7 +52,9 @@ function captureSingleVideo() {
   var FILE = `video_${TIMESTAMP}.h264`,
       COMMAND = `raspivid -o ${TEMP_STORAGE_DESTINATION}/${FILE} -t ${DURATION - 5000} -n -vs -st -b ${BITRATE} -ex night`;
 
-  console.log('Executing: ' + COMMAND);
+  console.log(`[${new Date()}] Executing: ${COMMAND}`);
+  saveFile = false;
+
   return cpp
     .exec(COMMAND)
     .then((result) => {
@@ -59,7 +68,6 @@ function captureSingleVideo() {
 
       if (saveFile) {
         copyFileToNas(FILE);
-        saveFile = false;
       }
 
       shell.exec(`rm -f ${TEMP_STORAGE_DESTINATION}/${FILE}`);
@@ -68,18 +76,13 @@ function captureSingleVideo() {
       console.log(`Javascript error: ${error}`);
     })
     .progress((cp) => {
-      console.log('PID: ', cp.pid);
+      console.log(`[${new Date()}] PID: ${cp.pid}`);
     });
-}
-
-function captureLoop() {
-  captureSingleVideo();
-  setInterval(captureSingleVideo, DURATION);
 }
 
 function copyFileToNas(fileName) {
   var copyFrom = `${TEMP_STORAGE_DESTINATION}/${fileName}`;
-  var copyTo = `${STORAGE_DESTINATION}/${filename}`;
+  var copyTo = `${STORAGE_DESTINATION}/${fileName}`;
 
   try {
     var stats = fs.statSync(copyFrom);
@@ -91,10 +94,10 @@ function copyFileToNas(fileName) {
     var start = Date.now();
     shell.exec(`cp ${copyFrom} ${copyTo}`, () => {
       var duration = (Date.now() - start) / 1000;
-      console.log(`File ${fileName} copied to ${STORAGE_DESTINATION}, took ${duration} seconds (${stats.size / 1024 / duration} KB/s).`)
+      console.log(`[${new Date()}] File ${fileName} copied to ${STORAGE_DESTINATION}, took ${duration} seconds (${(stats.size / 1024 / duration).toFixed()} KB/s).`)
     });
 
   } catch (err) {
-    console.error('Error copying file.', err);
+    console.error(`[${new Date()}] Error copying file.`, err);
   }
 }
