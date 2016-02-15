99f69dff76ea55f85e27650ff04ce5a3c672f011
Adam Sheldon <adam.sheldon@hotmail.com>
2016-02-14 18:11:18 -0800

    Added: temp storage, file copy, gpio detection.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var cpp = require('child-process-promise');
+var fs    = require('fs');
+var cpp   = require('child-process-promise');
+var gpio  = require('rpi-gpio');
+var shell = require('shelljs');
 
 var STORAGE_DESTINATION = '/mnt/nas',
+    TEMP_STORAGE_DESTINATION = './temp_vid/',
     DURATION = 10 * 60 * 1000,
     BITRATE = 2 * 1000 * 1000,
     DATE_SETTINGS = {
@@ -12,14 +16,34 @@ var STORAGE_DESTINATION = '/mnt/nas',
       year: 'numeric'
     };
 
-function captureVideo() {
+var motionDetected = false;
+
+// Setup the GPIO pins for motion detection
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.on('change', (chanel, value) => {
+  console.log(`[${new Date()}] Motion ${value ? 'detected.' : 'timed out.'}.`);
+  motionDetected = value;
+});
+
+// Find out how many ticks we are from the next 10 minute interval
+var offset = DURATION - (Date.now() % (DURATION));
+console.log(`Offset of ${offset / 1000} seconds detected, waiting...`);
+
+// Start!
+setTimeout(captureLoop, offset);
+
+/*
+ * Helper Functions
+ */
+
+function captureSingleVideo() {
   var TIMESTAMP = new Date()
     .toLocaleString('en-US', DATE_SETTINGS)
     .replace(/\:|\s|\//g, '')
     .replace(',', '_');
 
   var FILE = `video_${TIMESTAMP}.h264`,
-      COMMAND = `raspivid -o ${STORAGE_DESTINATION}/${FILE} -t ${DURATION - 5000} -n -vs -st -b ${BITRATE} -ex night`;
+      COMMAND = `raspivid -o ${TEMP_STORAGE_DESTINATION}/${FILE} -t ${DURATION - 5000} -n -vs -st -b ${BITRATE} -ex night`;
 
   console.log('Executing: ' + COMMAND);
   return cpp
@@ -32,6 +56,10 @@ function captureVideo() {
       if (result.stderr) {
         console.log(`Process err: ${result.stderr}`);
       }
+
+      if (motionDetected) {
+        copyFileToNas(FILE);
+      }
     })
     .catch((error) => {
       console.log(`Javascript error: ${error}`);
@@ -42,14 +70,28 @@ function captureVideo() {
 }
 
 function captureLoop() {
-  captureVideo();
-  
-  setInterval(captureVideo, DURATION);
+  captureSingleVideo();
+  setInterval(captureSingleVideo, DURATION);
 }
 
-// Find out how many ticks we are from the next 10 minute interval
-var offset = DURATION - (Date.now() % (DURATION));
-console.log(`Offset of ${offset / 1000} seconds detected, waiting...`);
+function copyFileToNas(fileName) {
+  var copyFrom = `${TEMP_STORAGE_DESTINATION}/${fileName}`;
+  var copyTo = `${STORAGE_DESTINATION}/${filename}`;
 
-setTimeout(captureLoop, offset);
+  try {
+    var stats = fs.statSync(copyFrom);
 
+    if (!stats.isFile()) {
+      throw new Error(`Not a valid file: ${copyFrom}`);
+    }
+
+    var start = Date.now();
+    shell.exec(`cp ${copyFrom} ${copyTo} && rm ${copyFrom}`, () => {
+      var duration = (Date.now() - start) / 1000;
+      console.log(`File ${fileName} copied to ${STORAGE_DESTINATION}, took ${duration} seconds (${stats.size / 1024 / duration} KB/s).`)
+    });
+
+  } catch (err) {
+    console.error('Error copying file.', err);
+  }
+}
